eba98538adf43364d7273969065e65922aec534b
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-06-10 15:50:54 -0700

    add some images

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
-  const handleOnPress = () => console.log("Text Clicked"); 
+  const handleOnPress = () => {console.log("Text Clicked")}; 
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={10} onPress={handleOnPress}>
         Big test for long text really long text going ham bruh let's doo this.
       </Text>
+      <Image source={require('./assets/favicon.png')} />
+      <Image blurRadius={1} source={{
+        width: 200,
+        height: 300,
+        uri:"https://picsum.photos/200/300"}} />
       <StatusBar style="auto" />
     </SafeAreaView>
   );

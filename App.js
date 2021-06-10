36c931d8317a554e2a89c3c68e70f667e6d03bde
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-06-10 15:56:14 -0700

    add touchablility to image

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handleOnPress = () => {console.log("Text Clicked")}; 
+  const handleOnPressI = () => {
+    console.log("Image Clicked");
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={10} onPress={handleOnPress}>
         Big test for long text really long text going ham bruh let's doo this.
       </Text>
       <Image source={require('./assets/favicon.png')} />
-      <Image blurRadius={1} source={{
-        width: 200,
-        height: 300,
-        uri:"https://picsum.photos/200/300"}} />
+      <TouchableOpacity onPress={handleOnPressI}>
+        <Image blurRadius={1} source={{
+          width: 200,
+          height: 300,
+          uri:"https://picsum.photos/200/300"}} />
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );

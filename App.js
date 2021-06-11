3a8afb51cb950d170d6d10108d4716f6f687601d
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-06-11 15:55:26 -0700

    add positioning properties

--- App.js
+++ App.js
@@ -13,33 +13,22 @@ export default function App() {
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300
+          height: 100
         }}
       />
       <View 
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 200,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: "absolute" //relative is relative to child comps, absolute is to parent, react native is default relative
         }}
       />
       <View 
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100
-        }}
-      />
-      <View 
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100
-        }}
-      />
-      <View 
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100
         }}
@@ -52,10 +41,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: "row", //horizontal/vertical
-    justifyContent: "center", // main axis
-    alignItems: "center", // secondary axis, within each line (in terms of wrapping)
-    alignContent: "center", // Secondary axis, everything, including each line
-    flexWrap: "wrap"
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });

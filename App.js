48aa7b97f67783fbfc187ab703286a3deb43462f
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-06-11 19:19:24 -0700

    refactor directory layout

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import React from 'react';
 //   StyleSheet, 
 //   View, 
 //   Image,} from 'react-native';
-import WelcomeScreen from "./screens/WelcomeScreen";
-import ViewImageScreen from "./screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <WelcomeScreen />
   );
 }

d46aa8807ee11537d73c29fd2749224606cd52e2
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-06-11 18:44:42 -0700

    add ViewImageScreen

---
+++ screens/ViewImageScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  View, 
+  Image,
+  Text} from 'react-native';
+
+export default function ViewImageScreen() {
+  return(
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "white"
+  }
+});
